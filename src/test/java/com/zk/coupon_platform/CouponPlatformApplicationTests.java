8b022240a44eea0957f58d58fc55f5686c783d85
zack <zack.j.hung@gmail.com>
2026-05-02 23:53:01 +0800

    init

---
+++ src/test/java/com/zk/coupon_platform/CouponPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zk.coupon_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
